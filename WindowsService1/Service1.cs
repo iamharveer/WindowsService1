f3a792ddeaf89161ad586b42654d2694b88c18ca
iamharveer <harveersingh@live.com>
2017-02-05 17:47:57 -0500

    My first commit

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private System.Diagnostics.EventLog eventLog1;
+
+        public Service1(string[] args)
+        { 
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart ");
+            // Set up a timer to trigger every minute.  
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000 * 60 * 6; // checks every 6 hours. 
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+
+            if (args == null || args.Length != 1)
+            {
+                throw new Exception("args should only contain the abs path to key file.");
+            }
+            cmdArgs = args;
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System"); //, "Information", EventLogEntryType.Information);
+
+            var CheckUpdates = new Updates("http://www.ontarioimmigration.ca/en/pnp/OI_PNPNEW.html");
+            if (CheckUpdates.PageIsDifferent)
+            {
+                CheckUpdates.SendMail(cmdArgs[0]);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In onStop.");
+        }
+
+        string[] cmdArgs = null;
+    }
+}
---
+++ WindowsService1/Updates.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace WindowsService1
+{
+    class Updates
+    {
+        public Updates(string URL)
+        {
+            var file = URL.Split('/');
+            file = (file[file.Length - 1]).Split('.');
+            var filename = file[0];
+
+            System.Net.WebClient wc = new System.Net.WebClient();
+            byte[] raw = wc.DownloadData(URL);
+
+            string newData = System.Text.Encoding.UTF8.GetString(raw);
+
+            if (File.Exists(filename))
+            {
+                var oldData = File.ReadAllText(filename);
+                PageIsDifferent = IsDifferent(newData, oldData);
+            }
+            else
+            {
+                using (var fs = File.Open(filename, FileMode.CreateNew))
+                {
+                    try
+                    {
+                        fs.Write(raw, 0, raw.Length);
+                    }
+                    catch(Exception ex)
+                    {
+                        System.Diagnostics.Debug.Assert(false, ex.Message);
+                    }
+                }
+            }
+        }
+
+        private bool IsDifferent(string oldData, string newData)
+        {
+            return string.Compare(oldData, newData) != 0;
+        }
+
+        public void SendMail(string credFilePath)
+        {
+            var fs = File.ReadAllLines(credFilePath);
+            if (fs.Length != 1)
+            {
+                System.Diagnostics.Debug.Assert(false, "Credential file contains more lines than expected.");
+            }
+
+            var credData = fs[0].Split(' ');
+            var senderEmail = credData[0];
+            var pwd = credData[1];
+            var receiverEmail = credData[2];
+
+            MailMessage mail = new MailMessage(senderEmail, receiverEmail);
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.live.com";
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(senderEmail, pwd);// New System.Net.NetworkCredential()
+            mail.Subject = "There are some changes in PNP webpage.";
+            mail.Body = "Please follow below link to review. <br> http://www.ontarioimmigration.ca/en/pnp/OI_PNPNEW.html";
+
+            client.Send(mail);
+        }
+
+        public bool PageIsDifferent = false;
+    }
+}
